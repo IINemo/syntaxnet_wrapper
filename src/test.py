e1322d72c3eeb8eb9898cc2e97e408cacb70b4be
Nemo <artem@localhost.localdomain>
2017-02-08 16:23:33 +0300

    q

--- src/test.py
+++ src/test.py
@@ -1,28 +1,42 @@
 # -*- coding: utf-8 -*-
 
 from syntaxnet_wrapper import ProcessorSyntaxNet
-import sys
-import argparse
+from syntaxnet_wrapper import PipelineSyntaxNet
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description = 'Wrapper tester.')
-    
-    parser.add_argument('--host', 
-                        required = True, 
-                        help = 'Host with syntaxnet server.')
-    parser.add_argument('--port',
-                        required = True,
-                        help = 'Syntaxnet server port.',
-                        default = 8111)
-    args = parser.parse_args()
-    
-    proc = ProcessorSyntaxNet(args.host, int(args.port))
-    
-    text = sys.stdin.read().decode('utf8').strip()
-    result = proc.parse(text)
-    
+def print_result(result):
     for sent in result:
         for word in sent:
             print word
         print
+
+
+if __name__ == '__main__':
+    import sys
+    import argparse
+    
+    parser = argparse.ArgumentParser(description = 'Wrapper tester.')
+    
+    parser.add_argument('--host', 
+                        required = True, 
+                        help = 'Host with SyntaxNet server.')
+    parser.add_argument('--port',
+                        required = True,
+                        help = 'SyntaxNet server port.',
+                        default = 8111)
+    parser.add_argument('--proc_type',
+                        required = True,
+                        help = '"proc" - processor or "ppl" - pipeline')
+    args = parser.parse_args()
+    
+    text = sys.stdin.read().decode('utf8').strip()
+    
+    if args.proc_type == 'proc':    
+        proc = ProcessorSyntaxNet(args.host, int(args.port))
+        result = proc.parse(text)
+        print_result(result)
+        
+    elif args.proc_type == 'ppl':
+        ppl = PipelineSyntaxNet(args.host, int(args.port))
+        result = ppl.process(text)
+        print_result(result)
